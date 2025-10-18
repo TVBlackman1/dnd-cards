7250a2b25335790f72cf1aab5ee15c0d99097ecc
tvblackman1 <vbif.misha2012@yandex.ru>
2025-10-18 18:56:44 +0300

    base

---
+++ instr.sh
@@ -0,0 +1,12 @@
+# LaTeX Workshop extention for vs code (wsl)
+
+sudo apt update && sudo apt upgrade
+sudo apt install texlive-full texlive-latex-extra texlive-xetex texlive-lang-cyrillic
+sudo apt install language-pack-ru
+
+sudo dpkg-reconfigure locales # select ru_RU.UTF-8
+export LANG=ru_RU.UTF-8
+
+sudo apt install ttf-mscorefonts-installer
+sudo apt install fonts-noto-cjk
+
